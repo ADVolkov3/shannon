ff8432cfa5483303650ef537f3a40344bf96c515
TheBatya3 <86134076+TheBatya3@users.noreply.github.com>
2022-12-16 21:38:20 +0300

    coder update

--- coder/coder.py
+++ coder/coder.py
@@ -1,10 +1,19 @@
 import zlib
 
-class Node:
+symbol_db = {}
+
+class Symbol:
     def __init__(self):
-        self.symbol = ''
+        self.name = ""
         self.probability = 0.0
         self.arr = [0] * 20
+        self.code = ""
+
+    def __str__(self):
+        return self.name
+class Node:
+    def __init__(self):
+        self.symbol = Symbol()
         self.top = 0
 
 def shannon(num: int, num_of_symbols: int, list_of_nodes: list[Node]):
@@ -14,14 +23,14 @@ def shannon(num: int, num_of_symbols: int, list_of_nodes: list[Node]):
         if num == num_of_symbols or num > num_of_symbols:
             return
         list_of_nodes[num_of_symbols].top += 1
-        list_of_nodes[num_of_symbols].arr[list_of_nodes[num_of_symbols].top] = 0
+        list_of_nodes[num_of_symbols].symbol.arr[list_of_nodes[num_of_symbols].top] = 0
         list_of_nodes[num].top += 1
-        list_of_nodes[num].arr[list_of_nodes[num].top] = 1
+        list_of_nodes[num].symbol.arr[list_of_nodes[num].top] = 1
         return
     else:
         for i in range(num, num_of_symbols):
-            pack1 = pack1 + list_of_nodes[i].probability
-        pack2 = pack2 + list_of_nodes[num_of_symbols].probability
+            pack1 = pack1 + list_of_nodes[i].symbol.probability
+        pack2 = pack2 + list_of_nodes[num_of_symbols].symbol.probability
         diff1 = pack1 - pack2
         if diff1 < 0:
             diff1 = diff1 * -1
@@ -31,9 +40,9 @@ def shannon(num: int, num_of_symbols: int, list_of_nodes: list[Node]):
             k = num_of_symbols - j
             pack1 = pack2 = 0
             for i in range(num, k + 1):
-                pack1 = pack1 + list_of_nodes[i].probability
+                pack1 = pack1 + list_of_nodes[i].symbol.probability
             for i in range(num_of_symbols, k, -1):
-                pack2 = pack2 + list_of_nodes[i].probability
+                pack2 = pack2 + list_of_nodes[i].symbol.probability
             diff2 = pack1 - pack2
             if diff2 < 0:
                 diff2 = diff2 * -1
@@ -45,11 +54,11 @@ def shannon(num: int, num_of_symbols: int, list_of_nodes: list[Node]):
             k += 1
         for i in range(num, k + 1):
             list_of_nodes[i].top += 1
-            list_of_nodes[i].arr[list_of_nodes[i].top] = 1
+            list_of_nodes[i].symbol.arr[list_of_nodes[i].top] = 1
 
         for i in range(k + 1, num_of_symbols + 1):
             list_of_nodes[i].top += 1
-            list_of_nodes[i].arr[list_of_nodes[i].top] = 0
+            list_of_nodes[i].symbol.arr[list_of_nodes[i].top] = 0
 
         # Invoke shannon function
         shannon(num, k, list_of_nodes)
@@ -60,25 +69,26 @@ def sortByProbability(num_of_symbols: int, list_of_nodes: list[Node]) -> None:
     temp = Node()
     for j in range(1, num_of_symbols):
         for i in range(num_of_symbols - 1):
-            if list_of_nodes[i].probability > list_of_nodes[i + 1].probability:
-                temp.probability = list_of_nodes[i].probability
-                temp.symbol = list_of_nodes[i].symbol
+            if list_of_nodes[i].symbol.probability > list_of_nodes[i + 1].symbol.probability:
+                temp.probability = list_of_nodes[i].symbol.probability
+                temp.symbol.name = list_of_nodes[i].symbol.name
 
-                list_of_nodes[i].probability = list_of_nodes[i + 1].probability
-                list_of_nodes[i].symbol = list_of_nodes[i + 1].symbol
+                list_of_nodes[i].symbol.probability = list_of_nodes[i + 1].symbol.probability
+                list_of_nodes[i].symbol.name = list_of_nodes[i + 1].symbol.name
 
-                list_of_nodes[i + 1].probability = temp.probability
-                list_of_nodes[i + 1].symbol = temp.symbol
+                list_of_nodes[i + 1].symbol.probability = temp.probability
+                list_of_nodes[i + 1].symbol.name = temp.symbol.name
 
 
-def display(number_of_symbols, nodes_list: list[Node]) -> None:
-    res: str = ""
+def display(number_of_symbols: int, nodes_list: list[Node]) -> None:
+    print("\n\n\n\tSymbol\tProbability\tCode", end='')
     for i in range(number_of_symbols - 1, -1, -1):
+        print("\n\t", nodes_list[i].symbol, "\t\t", nodes_list[i].symbol.probability, "\t", end='')
         for j in range(nodes_list[i].top + 1):
-            res += str(nodes_list[i].arr[j])
-    print(res)
-    with open("result.txt", "wb") as f:
-        f.write(zlib.compress(res.encode("utf-8")))
+            symbol_db[nodes_list[i].symbol.name] = nodes_list[i].symbol
+            nodes_list[i].symbol.code += str(nodes_list[i].symbol.arr[j])
+            print(nodes_list[i].symbol.arr[j], end='')
+    print("\n")
 
 def main():
     total = 0
@@ -89,11 +99,11 @@ def main():
     num_of_symbols = len(symbols)
     nodes = [Node() for _ in range(num_of_symbols)]
     for i in range(num_of_symbols):
-        nodes[i].symbol += symbols[i]
+        nodes[i].symbol.name += symbols[i]
 
     for i in range(num_of_symbols):
-        nodes[i].probability = float(symb_probabilities[i])
-        total = total + nodes[i].probability
+        nodes[i].symbol.probability = float(symb_probabilities[i])
+        total = total + nodes[i].symbol.probability
 
         if total > 1:
             print("Invalid. Enter new values")
@@ -107,6 +117,12 @@ def main():
     shannon(0, num_of_symbols - 1, nodes)
 
     display(num_of_symbols, nodes)
+    to_encode = input()
+    result = ""
+    for symb in to_encode:
+        result += symbol_db[symb].code
+    with open("result.txt", "wb") as f:
+        f.write(zlib.compress(result.encode("utf-8")))
 
 
 if __name__ == '__main__':
